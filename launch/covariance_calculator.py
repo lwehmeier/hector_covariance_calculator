b5611220468dedcc3b61aac6b5c8d07aea2ccf83
Leon Wehmeier <lw96@st-andrews.ac.uk>
2018-07-23 18:58:56 +0100

    launch file name

--- launch/covariance_calculator.py
+++
@@ -1,8 +0,0 @@
-<!-- Starts up everything needed to run the example listener/talker Python nodes. -->
-
-<launch>
-  <node pkg="hector_covariance_calculator" type="covariance_inverter.py" name="hector_covariance_calculator">
-    <param name="rate" value="10"/>
-  </node>
-
-</launch>
